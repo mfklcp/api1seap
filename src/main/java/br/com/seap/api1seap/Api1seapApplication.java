0b80dc347c20bde11a46f65adcb7ceb136f40e7d
Franklin <mfklcp@gmail.com>
2021-02-12 17:14:19 -0300

    first commit

---
+++ src/main/java/br/com/seap/api1seap/Api1seapApplication.java
@@ -0,0 +1,13 @@
+package br.com.seap.api1seap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1seapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api1seapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/seap/api1seap/Api1seapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seap.api1seap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api1seapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
